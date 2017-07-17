6f572ad05ac00cb53f86c49fde350f2ff134efc4
J. Carlos Urteaga Reyesvera <carlos@JCs-Macbook-Pro.local>
2017-07-17 00:21:26 -0500

    adding url for the Tiles

--- Interview/FibonacciWords/Ex2.m
+++ Interview/FibonacciWords/Ex2.m
@@ -15,21 +15,21 @@
 	#import <Foundation/Foundation.h>
 
 
-NSString* FibbonacciWord(int number);
+NSString* FibonacciWord(int number);
 int main (int argc, const char * argv[]){
 
 	if (argc ==3){
 		NSString *strNumber = [NSString stringWithUTF8String:argv[1]];
 		NSString *strPatron = [NSString stringWithUTF8String:argv[2]];
-		int intFibbonaci;
+		int intFibonaci;
 		if ([strNumber intValue] != 0)
 		{
-			intFibbonaci = [strNumber intValue];
+			intFibonaci = [strNumber intValue];
 			//printf("%i\n", [strNumber intValue]);
-			if (intFibbonaci<40)
+			if (intFibonaci<40)
 			{
-				NSLog(@"Fibbonacci: %@",FibbonacciWord(intFibbonaci));
-				NSString *string = FibbonacciWord(intFibbonaci);
+				NSLog(@"Fibonacci: %@",FibonacciWord(intFibonaci));
+				NSString *string = FibonacciWord(intFibonaci);
 				NSError *error = NULL;
 				NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:strPatron options:NSRegularExpressionCaseInsensitive error:&error];
 				NSUInteger numberOfMatches = [regex numberOfMatchesInString:string options:0 range:NSMakeRange(0, [string length])];
@@ -38,22 +38,22 @@ int main (int argc, const char * argv[]){
 		}
 	}
 	else{
-		printf("Error, the program requiere two parameters. The first is the Fibbonacci number, and the second is the pattern\n");
+		printf("Error, the program requiere two parameters. The first is the Fibonacci number, and the second is the pattern\n");
 	}
 	
 	return 0;
 }
 
 
-NSString* FibbonacciWord(int number){
+NSString* FibonacciWord(int number){
 		if(number ==0)
 			return @"0";
 		if (number ==1)
 		{
 			return @"1";
 		}else
-			return [NSString stringWithFormat: @"%@%@", FibbonacciWord(number -1), FibbonacciWord(number-2)];
-			//[FibbonacciWord(number -1) + FibbonacciWord(number-2);
+			return [NSString stringWithFormat: @"%@%@", FibonacciWord(number -1), FibonacciWord(number-2)];
+			//[FibonacciWord(number -1) + FibonacciWord(number-2);
 		
 
 	}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+#Interviews Questions
+
+The idea is generate a Repo to practice and show my habilities. 
+
+The Interview questions are in the folder Interview. In side it, each question has it own folder.
+
+My goal will to have the solution of each question in my favorite language such as C, Python, Objective C and JAVA .
+
