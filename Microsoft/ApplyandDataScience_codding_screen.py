67b2ae2529870de05745efd84957c1554c4c671e
Jorge Carlos Urteaga Reyesvera <jurteagar@gmail.com>
2020-09-10 20:22:10 -0500

    fixing reality vs solution

--- Microsoft/ApplyandDataScience_codding_screen.py
+++ Microsoft/ApplyandDataScience_codding_screen.py
@@ -17,8 +17,8 @@ def string_to_number(string):
 # the next possible question
 # complexity of the funciton is nlog(n)
 # does this not work for floats? what do you need require to add the next code.
-
-def string_to_decimals(string):
+# str_to_int
+def str_to_int(string):
 	real_value = 0
 	size = 1
 	for each in string:
@@ -30,7 +30,7 @@ def string_to_decimals(string):
 def string_to_float(string):
 	strings = string.split('.')
 	number = string_to_number(strings[0])
-	number += string_to_decimals(strings[1])
+	number += str_to_int(strings[1])
 	return number
 
 print(string_to_float('1234.1234'))
@@ -57,6 +57,39 @@ def k_most(k, array):
 # third question
 # talk 
 # how to know if a value exist on a list, the list is ordered (ascending)
+"""
+online coding (wrong)
+# you can write to stdout for debugging purposes, e.g.
+print("This is a debug message")
+
+# len 10
+# size 10
+# 5 / 2.5
+# sorted ascending
+def if_exit(value, array):
+    size = len(array) 
+    mid = int(size/2) 
+    # if mid == 0 or value== array[mid]:
+    if len(array)==1:
+        print('here', mid)
+        if array[mid]== value:
+            print( 'True')
+            return 0
+        else:
+            print( 'False')
+            return 0
+    if (value>array[mid]):
+            if_exit(value,array[mid:])
+    else:
+        if value<array[mid]:
+            if_exit(value,array[:mid])
+
+# 2 4 6 8 (5)...
+array = [2,4,6,8]
+value = 1
+
+print(if_exit(value, array))
+"""
 def return_if_exist(array, value):
 	mid = int(len(array) / 2)
 	if array[mid] == value:
